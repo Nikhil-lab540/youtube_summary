ff5be156e8d28e1708d79a46b22cb2598aaf3496
Kaveti Nikhil <nikhilgithub540@gmail.com>
2024-10-15 15:25:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -37,6 +37,21 @@ def generate_gemini_content(transcript_text,prompt):
     response=model.generate_content(prompt+transcript_text)
     return response.text
 
+
+hide_st_style = """
+            <style>
+            /* Hide Streamlit's default hamburger menu */
+            #MainMenu {visibility: hidden;}
+            
+            /* Hide Streamlit's footer (the "Made with Streamlit" footer) */
+            footer {visibility: hidden;}
+            
+            /* Hide Streamlit's "Deploy" button */
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
 st.title("YouTube Transcript to Detailed Notes Converter")
 youtube_link = st.text_input("Enter YouTube Video Link:")
 
